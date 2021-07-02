69e8188fb7812512ba75790484f2c6f5f0ca0cf6
EMachad0 <elitonmachadod200@gmail.com>
2021-07-02 00:33:41 -0300

    sorting and name filter

--- blueprints/collection.py
+++ blueprints/collection.py
@@ -1,8 +1,31 @@
-from flask import Blueprint, render_template
+import json
+from flask import Blueprint, redirect, url_for, render_template
+
+from notebooks import db
 
 blue = Blueprint('collection', __name__, static_folder="static", template_folder="templates")
 
+# http://localhost:5000/collection/823962832583655424/203615933539942400#
+
+PLAYER_SELECT = "select player_cod from player where user_cod=%s and server_cod=%s;"
+COLLECTION_SELECT = "select * from (select pull_cod from collection where player_cod=%s) c " \
+                    "join pull p on p.pull_cod = c.pull_cod " \
+                    "join card cd on cd.card_cod = p.card_cod;"
 
 @blue.route('/<guild>/<user>')
 def collection(guild, user):
-    return render_template('collection.html', guild=guild, user=user)
+    player = db.make_select(PLAYER_SELECT, (user, guild))
+    if len(player) == 0:
+        return redirect(url_for("login.login"))
+    player = player[0]
+    
+    cards = db.make_select(COLLECTION_SELECT, [player['player_cod']])
+    cards_json = {c['card_cod']: c for c in cards}
+    print(cards)
+    return render_template('collection.html', cards=cards, cards_json=cards_json, guild=guild, user=user)
+
+
+if __name__ == "__main__":
+    car = db.make_select(COLLECTION_SELECT, [7])
+    car = {c['card_cod']:c for c in car}
+    print(car)
