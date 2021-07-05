d649ec44dec380c635446ac70255e9c4092e3bf4
EMachad0 <elitonmachadod200@gmail.com>
2021-07-04 23:26:35 -0300

    to lower case

--- static/js/card_filter.js
+++ static/js/card_filter.js
@@ -13,7 +13,7 @@ function do_filter(list) {
     toFilter = Array.prototype.slice.call(toFilter, 0);
     toFilter.forEach(function (v) {
         let card = card_values[v.id];
-        if (name != null && name.value !== "" && !card['name'].includes(name.value)) hide(v);
+        if (name != null && name.value !== "" && !card['name'].toLowerCase().includes(name.value.toLowerCase())) hide(v);
         else show(v);
     });
 }
