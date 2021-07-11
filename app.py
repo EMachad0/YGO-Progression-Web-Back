e0f110d315027d9d30f9cf0d7390bd790b18fdf2
EMachad0 <elitonmachadod200@gmail.com>
2021-07-11 14:26:13 -0300

    secret key

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.secret_key = os.environ['SECRET_KEY']
+app.secret_key = 'Machado'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
