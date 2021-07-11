1073c9c4b5ea9b3c81ee9e56c033369795947aa2
EMachad0 <elitonmachadod200@gmail.com>
2021-07-11 14:30:23 -0300

    heroku quebrou

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = 'Machado'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://gbbukjnnxeolsm:ac0c65368870ba42501c292c93c21a96949c23837b0ab5c3102ce6e5819502f7@ec2-52-6-77-239.compute-1.amazonaws.com:5432/d7msjjetg6jff5'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
