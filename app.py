fcd41ff863a96a98b45960e6c4d1cc73bf9a864d
EMachad0 <elitonmachadod200@gmail.com>
2021-07-11 15:17:39 -0300

    CORS

--- app.py
+++ app.py
@@ -2,12 +2,16 @@ import os
 
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 app.secret_key = 'Machado'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://gbbukjnnxeolsm:ac0c65368870ba42501c292c93c21a96949c23837b0ab5c3102ce6e5819502f7@ec2-52-6-77-239.compute-1.amazonaws.com:5432/d7msjjetg6jff5'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+CORS(app)
+
 
 import blueprints
 app.register_blueprint(blueprints.collection)
