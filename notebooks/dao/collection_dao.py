d6444005c888e78b989592b3adbfa4e8dc82658c
EMachad0 <elitonmachadod200@gmail.com>
2021-07-11 00:52:30 -0300

    more fields

--- notebooks/dao/collection_dao.py
+++ notebooks/dao/collection_dao.py
@@ -13,10 +13,11 @@ class Collection(db.Model):
 def get_player_collection(player_cod, offset=None, limit=None, name_filter=None, sorts=None, filters=None):
     from notebooks.filters import apply_sort, apply_filter
     from notebooks.dao import Pull, Card
-    
-    query = db.session.query(Card.card_cod, Card.name, Card.atk, Card.level, Card.cod_img, Collection.quantity)\
-        .join(Pull, Collection.pull_cod==Pull.pull_cod)\
-        .join(Card, Pull.card_cod==Card.card_cod)\
+
+    query = db.session.query(Card.card_cod, Card.name, Card.atk, Card.level, Card.cod_img, Card.type, Card.level,
+                             Card.scale, Card.link_val, Card.race, Card.attribute, Collection.quantity) \
+        .join(Pull, Collection.pull_cod == Pull.pull_cod) \
+        .join(Card, Pull.card_cod == Card.card_cod) \
         .filter(Collection.player_cod == player_cod)
     if name_filter is not None:
         query = query.filter(Card.name.ilike(name_filter))
