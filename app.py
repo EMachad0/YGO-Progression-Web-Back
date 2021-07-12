a4bd24407598a15e69f89789a8bb0801d1f13ecf
EMachad0 <elitonmachadod200@gmail.com>
2021-07-11 22:33:53 -0300

    os environ

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import os
 
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 from flask_cors import CORS
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.secret_key = 'Machado'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://gbbukjnnxeolsm:ac0c65368870ba42501c292c93c21a96949c23837b0ab5c3102ce6e5819502f7@ec2-52-6-77-239.compute-1.amazonaws.com:5432/d7msjjetg6jff5'
+app.secret_key = os.environ['SECRET_KEY']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 CORS(app)
