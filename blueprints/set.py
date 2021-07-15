f1c36f47f465fc325d94a697d4039fae0457b905
Machado <elitonmachadod200@gmail.com>
2021-07-15 17:16:57 -0300

    plaeyr sets

--- blueprints/set.py
+++ blueprints/set.py
@@ -1,10 +1,26 @@
 import json
 
-from flask import Blueprint
+from flask import Blueprint, request
 
-from notebooks.dao import set_dao, utils
+from notebooks import utils
+from notebooks.dao import set_dao, player_dao
 
 blue = Blueprint('set', __name__, static_folder="static", template_folder="templates")
+
+
+@blue.route('/set/player')
+def player_sets():
+    params = request.args
+    if params.get('guild') is None or params.get('user') is None:
+        return "{'error': 'No User or guild'}"
+
+    player = player_dao.get_player_by_user_server(params['user'], params['guild'])
+    if player is None:
+        return "{'error': 'Invalid Player'}"
+
+    sets = set_dao.get_player_sets(player.player_cod)
+    sets = [utils.row2dict(s) for s in sets]
+    return json.dumps(sets)
 
 
 @blue.route('/set/all_sets')
--- notebooks/dao/set_dao.py
+++ notebooks/dao/set_dao.py
@@ -9,9 +9,16 @@ class Set(db.Model):
     type_cod = db.Column(db.Integer, db.ForeignKey('set_type.set_type_cod'))
 
 
-def get_all_sets():
-    return db.session.query(Set).all()
-
-
 def get_set(set_cod):
     return db.session.query(Set).get(set_cod)
+
+
+def get_all_sets():
+    return db.session.query(Set).order_by(Set.release_date).all()
+
+
+def get_player_sets(player_cod):
+    from notebooks.dao import Collection, Pull
+    sub = db.session.query(Pull.set_cod).join(Collection, Pull.pull_cod == Collection.pull_cod) \
+        .filter(Collection.player_cod == player_cod).subquery()
+    return db.session.query(Set).join(sub, Set.set_cod == sub.c.set_cod).order_by(Set.release_date).all()
